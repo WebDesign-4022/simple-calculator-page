5ce3743c08b9e8421ac7ea837a8edcb511848dc5
dshmmd <mohammadzarea.bidaki@gmail.com>
2024-03-09 23:11:09 +0330

    initial base springboot project

---
+++ SimpleCalculator/src/main/java/com/webdesign/simplecalculator/SimpleCalculator/SimpleCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.webdesign.simplecalculator.SimpleCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCalculatorApplication.class, args);
+	}
+
+}
